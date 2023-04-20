af455c958350b8231657429f7da83cbe1946c650
SebastianRojas2004 <chevyrojasa@gmail.com>
2023-04-20 17:43:51 -0500

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,85 +1,149 @@
-let producto = ['No a escogido ningun producto','Computador', 'Portatil', 'PlayStation', 'Xbox','Nintendo'];
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector('.container-cart-products');
 
-const productosComprar = new Array();     
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart');
+})
 
-    let respuesta = true;
+const cartInfo = document.querySelector('.cart-product');
 
-    for (respuesta; respuesta == true;){
-    resultadoProducto = window.prompt('¿Que producto desea comprar? \n 1.Computador \n 2.Portatil \n 3.PlayStation \n 4.Xbox \n 5.Nintendo');
-    
-        if(resultadoProducto == 0){
-            window.alert('Escoga por favor un producto');
-        }else if(resultadoProducto == 1){
-            insertarProducto = producto[1];
-            productosComprar.push(insertarProducto);
-            respuesta = window.confirm('¿Quiere comprar otro producto?');
-        }else if(resultadoProducto == 2){
-            insertarProducto = producto[2];
-            productosComprar.push(insertarProducto);
-            respuesta = window.confirm('¿Quiere comprar otro producto?');
-        }else if(resultadoProducto == 3){
-            insertarProducto = producto[3];
-            productosComprar.push(insertarProducto);
-            respuesta = window.confirm('¿Quiere comprar otro producto?');
-        }else if(resultadoProducto == 4){
-            insertarProducto = producto[4];
-            productosComprar.push(insertarProducto);
-            respuesta = window.confirm('¿Quiere comprar otro producto?');
-        }else if(resultadoProducto == 5){
-            insertarProducto = producto[5];
-            productosComprar.push(insertarProducto);
-            respuesta = window.confirm('¿Quiere comprar otro producto?');
+const rowProduct = document.querySelector('.row-product');
+
+/*Lista de todos los contenedores de productos*/
+const productList = document.querySelector('.container-items');
+
+/* Variable de arreglo de producto*/
+
+let allProducts = [];
+const valorTotal = document.querySelector('.total-pagar');
+const countProducts = document.querySelector('#contador-productos')
+
+productList.addEventListener('click', e => {
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElement;
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent,
         }
+
+        const exits = allProducts.some(product => product.title === infoProduct.title)
+
+        if(exits){
+            const products = allProducts.map(product => {
+                if(product.title === infoProduct.title){
+                    product.quantity++;
+                    return product;
+                }else{
+                    return product;
+                }
+            })
+        }else {
+            allProducts = [...allProducts,infoProduct]
+        }
+
+        show();
     }
-    
-    let unicosElementos = [];
-    let contadorDeVecesrepetidas = [];
-    let contador = 1;
+})
 
-    for (let i = 0; i < productosComprar.length; i++){
-        if (productosComprar[i+1] === productosComprar[i]){
-            contador++;
-        }else{
-            unicosElementos.push(productosComprar[i]);
-            contadorDeVecesrepetidas.push(contador);
-            contador = 1;
-        }
+rowProduct.addEventListener('click',(e) => {
+    if(e.target.classList.contains('icon-close')){
+        const product = e.target.parentElement;
+        const title = product.querySelector('p').textContent;
+
+        allProducts = allProducts.filter(product => product.title !== title)
     }
 
-    let Subtotal1 = 0;
-    let Subtotal2 = 0;
-    let Subtotal3 = 0;
-    let Subtotal4 = 0;
-    let Subtotal5 = 0;
+    console.log(allProducts);
+    show();
+})
 
-    for (let c = 0; c < unicosElementos.length; c++) {
-        console.log('El valor de ' + unicosElementos[c] + ' se repite ' + contadorDeVecesrepetidas[c]);
-            if(contiene = unicosElementos[c].includes('Computador')){        
-                Computador = '2500000' * 1.25; //1.25 es el iva
-                Subtotal1 = Computador * contadorDeVecesrepetidas[c];
-                console.log(Subtotal1);
-            }
-            if(contiene = unicosElementos[c].includes('Portatil')){        
-                Portatil = '1500000' * 1.25; //1.25 es el iva
-                Subtotal2 = Portatil * contadorDeVecesrepetidas[c];
-                console.log(Subtotal2);
-            }
-            if(contiene = unicosElementos[c].includes('PlayStation')){        
-                PlayStation = '3000000' * 1.25; //1.25 es el iva
-                Subtotal3 = PlayStation * contadorDeVecesrepetidas[c];
-                console.log(Subtotal3);
-            }
-            if(contiene = unicosElementos[c].includes('Xbox')){        
-                Xbox = '2800000' * 1.25; //1.25 es el iva
-                Subtotal4 = Xbox * contadorDeVecesrepetidas[c];
-                console.log(Subtotal4);                
-            }
-            if(contiene = unicosElementos[c].includes('Nintendo')){        
-                Nintendo = '1800000' * 1.25; //1.25 es el iva
-                Subtotal5 = Nintendo * contadorDeVecesrepetidas[c];
-                console.log(Subtotal5);                            
-            }                    
-        }
+// funcion para mostrar en la pagina (HTML)
+const show = () => {
 
-        total = Subtotal1 + Subtotal2 + Subtotal3 + Subtotal4 + Subtotal5;
-        console.log('el resultado a pagar es ' + total);
+    if(!allProducts.length){
+        containerCartProducts.innerHTML = `<p class ="cart-empty">El carrito esta vacio`
+    }
+
+    let total = 0;
+    let totalOfProducts = 0;
+
+    rowProduct.innerHTML = '';
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div')
+        containerProduct.classList.add('cart-product')
+
+        containerProduct.innerHTML = `
+        <div class="info-cart-product">
+            <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+            <span class="precio-producto-carrito">${product.price}</span>
+            </span>
+        </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close">
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+            </svg>`;
+
+        rowProduct.append(containerProduct);
+
+        total = total + parseInt(product.quantity * product.price.slice(1));
+        totalOfProducts = totalOfProducts + product.quantity;
+    });
+
+    valorTotal.innerText = `€${total} M`;
+    countProducts.innerText = totalOfProducts;
+}
+
+let table = [
+];
+
+window.onload = cargarEvento;
+
+function cargarEvento(){
+    document.getElementById('btn-pagar').addEventListener('click',mostrarTarjeta,false)
+    document.getElementById('datosCliente').addEventListener('submit',datosCliente,false);
+}
+
+function mostrarTarjeta(){
+    let tablebody = document.getElementById('tabla-tarjeta');
+    let tablallena = '';
+
+    for (let i = 0; i < table.length; i++) {
+            tablallena += '<tr><td>' + table[i].Banco + '</td><td>' + table[i].Nombre + '</td><td>' + table[i].Numero + '</td><td>' + table[i].Fecha + '</td><td>' + table[i].Codigo + '</td><td>' + table[i].Empresa + '</td><td>' + table[i].Pais + '</td></tr>';
+    }
+
+    tablebody.innerHTML = tablallena;
+}
+
+function datosCliente(event){
+    event.preventDefault();
+
+    let BancoUsuario = document.getElementById('Banco').value;
+    let NombreUsuario = document.getElementById('Nombre').value;
+    let NumeroUsuario = document.getElementById('Numero').value;
+    let FechaUsuario = document.getElementById('Fecha').value;
+    let CodigoUsuario = document.getElementById('Codigo').value;
+    let EmpresaUsuario = document.getElementById('Empresa').value;
+    let PaisUsuario = document.getElementById('Pais').value;
+
+    let datosCliente = {Banco: BancoUsuario, Nombre: NombreUsuario, Numero: NumeroUsuario, Fecha: FechaUsuario, Codigo: CodigoUsuario, Empresa: EmpresaUsuario, Pais: PaisUsuario};
+    table.push(datosCliente);
+}
+
+window.addEventListener('load',function(){
+    document.getElementById('btn-pagar').addEventListener('click',function(){
+        alert('Compra realizada con exito, para validar los datos guardados vuelva a ejecutar el voton de pagar');
+    })
+})
